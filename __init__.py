a013dd15d274824f99ff7f9ec465532a7a932989
tbell83 <tbell@tombellnj.com>
2016-04-03 16:51:09 -0400

    syntax

--- __init__.py
+++ __init__.py
@@ -12,37 +12,37 @@ app = Flask(__name__)
 api = Api(app)
 
 def getMlsUrl(mlsId):
-    Url = 'http://www.realtor.com/realestateandhomes-search?mlslid={0}'.format(mlsId)
-    userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0'
-    headers = {'User-Agent': userAgent}
-    r = requests.get(Url, headers=headers)
-    return r.url
+  Url = 'http://www.realtor.com/realestateandhomes-search?mlslid={0}'.format(mlsId)
+  userAgent = 'Mozilla/5.0 (X11; Linux x86_64; rv:21.0) Gecko/20100101 Firefox/21.0'
+  headers = {'User-Agent': userAgent}
+  r = requests.get(Url, headers=headers)
+  return r.url
 
 
 class SomeBot(Resource):
-    def post(self):
-		try:
-			responseText = request.form['token']
-		except:
-			return Response(status=400)
+  def post(self):
+    try:
+      responseText = request.form['token']
+    except:
+      return Response(status=400)
 
-        responseText = 'Testing'
-		resp_json = {"response_type": "in_channel","text": responseText,"attachments": [{"text":"Partly cloudy today and tomorrow"}]}
-		return resp_json
+    responseText = 'Testing'
+    resp_json = {"response_type": "in_channel","text": responseText,"attachments": [{"text":"Partly cloudy today and tomorrow"}]}
+    return resp_json
 
 class GitUpdate(Resource):
-	def post(self):
-		try:
-            #comment for testing
-		    subprocess.Popen(['git', 'pull', 'origin', 'master'],cwd='/home/tbell/slack.tombell.io/slack_api')
-            subprocess.Popen(['touch','restart.txt'],cwd='/home/tbell/slack.tombell.io/tmp/')
-		    return jsonify({'msg': 'Success'})
-		except subprocess.CalledProcessError as error:
-		    return jsonify({'msg': 'Failure'})
+  def post(self):
+    try:
+      #comment for testing
+      subprocess.Popen(['git', 'pull', 'origin', 'master'],cwd='/home/tbell/slack.tombell.io/slack_api')
+      subprocess.Popen(['touch','restart.txt'],cwd='/home/tbell/slack.tombell.io/tmp/')
+      return jsonify({'msg': 'Success'})
+    except subprocess.CalledProcessError as error:
+      return jsonify({'msg': 'Failure'})
 
 
 api.add_resource(SomeBot, '/')
 api.add_resource(GitUpdate, '/git')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  app.run(debug=True)
